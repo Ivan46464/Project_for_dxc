89399d2257e9ffbef60def13b5c5085034119055
Ivan46464 <113828394+Ivan46464@users.noreply.github.com>
2023-03-17 17:26:41 +0200

    Add files via upload

---
+++ mysite/vsetaq/url.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+from register import views as v
+urlpatterns = [
+    path('<int:id>', views.index, name="index"),
+    path("", views.home, name="home"),
+    path("create/", views.create, name="create"),
+
+
+]
