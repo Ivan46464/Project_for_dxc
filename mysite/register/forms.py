097dd979578578cde5a63f5b7ab04cb8aa18c869
Ivan46464 <113828394+Ivan46464@users.noreply.github.com>
2023-04-07 12:37:26 +0300

    change

--- mysite/register/forms.py
+++ mysite/register/forms.py
@@ -1,9 +1,9 @@
 from typing import Type
 
-from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
 from django.contrib.auth.models import User
-
+from .models import Profile
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -11,3 +11,10 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["email", "username", "password1", "password2"]
+
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = '__all__'
